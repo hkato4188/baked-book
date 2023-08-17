fc005f73c6f8a38e192071dc944c10c95a36a98a
Hiroki Kato <hirokikato1@gmail.com>
2023-08-16 17:29:24 -0700

    truncate description to 200 characters, itemize ingredients and method directions into a list

--- src/components/DessertCard.js
+++ src/components/DessertCard.js
@@ -3,18 +3,19 @@ import "./css/DessertCard.css";
 
 function DessertCard({ dessert }) {
   const { id, Image, Name, Source, Description } = dessert;
-  const Method = dessert.Method;
-  const Ingredients = dessert.Ingredients;
-
-  console.log(Ingredients);
+  const Method = dessert.Method.map((step) => <li>{step}</li>);
+  const Ingredients = dessert.Ingredients.map((ingredient) => (
+    <li>{ingredient}</li>
+  ));
 
   return (
     <div className="dessert-card">
       <img className="dessert-image" src={Image} alt={Name} />
       <h4>{Name}</h4>
-      <h5>{Description}</h5>
-      <p>{Ingredients}</p>
-      <p>{Method}</p>
+      <h5>{`${Description.substring(0, 200)} ...`}</h5>
+      {/* <ol>{Ingredients}</ol> */}
+      {/* <ul>{Method}</ul> */}
+
       <button>
         <a href={Source}>Learn More</a>
       </button>
